05b23a720c4cc72a792e27e2ee2f92bb2f53ec95
Athul Sathyan <athulsathyan136@gmail.com>
2026-08-16 08:23:36 +0530

    Weather Advisor

---
+++ 20_weather_advisor.py
@@ -0,0 +1,10 @@
+print("🌦️ Weather Outfit Advisor")
+print("--------------------------")
+
+temperature = float(input("Enter temperature (°C): "))
+rain = input("Is it raining? (yes/no): ")
+
+print("\nYour Recommendation:")
+print("--------------------")
+print(f"Temperature: {temperature}°C")
+print(f"Raining: {rain}")
